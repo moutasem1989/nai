b27a0ec31658c5a6e811144abed1d45a7f5e51be
Moutasem Al Khnaifes <moutasem@al-khnaifes.de>
2025-04-13 23:00:11 +0200

    Initial Commit

---
+++ nai/naiApp.swift
@@ -0,0 +1,17 @@
+//
+//  naiApp.swift
+//  nai
+//
+//  Created by Moutasem Al Khnaifes on 13.04.25.
+//
+
+import SwiftUI
+
+@main
+struct naiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ naiTests/naiTests.swift
@@ -0,0 +1,17 @@
+//
+//  naiTests.swift
+//  naiTests
+//
+//  Created by Moutasem Al Khnaifes on 13.04.25.
+//
+
+import Testing
+@testable import nai
+
+struct naiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
